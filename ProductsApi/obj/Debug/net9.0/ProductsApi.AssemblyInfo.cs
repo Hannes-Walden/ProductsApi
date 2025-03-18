8285c8e88581efd5b71e426d860c270da3254af4
Hannes-Walden <hannes.walden@outlook.com>
2025-03-18 16:33:44 +0200

    Run Api and Frontend success.

--- ProductsApi/obj/Debug/net9.0/ProductsApi.AssemblyInfo.cs
+++ ProductsApi/obj/Debug/net9.0/ProductsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c08f43f33846a7642d3766981f1be380771b54c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5eb1854eb1f75405cc14c4417725bca80cd1ffd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
