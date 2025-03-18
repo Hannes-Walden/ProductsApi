7d190ad884cd7bfa28b1eb12fb3bc1e93239e84b
Hannes-Walden <hannes.walden@outlook.com>
2025-03-18 13:14:20 +0200

    Dotnet build succesful.

--- ProductsApi/Program.cs
+++ ProductsApi/Program.cs
@@ -1,2 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using ProductsApi.Data;
+
+
+
+
+var builder = WebApplication.CreateBuilder();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseInMemoryDatabase("ProductsDb"));
